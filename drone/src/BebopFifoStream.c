2bbdd0b3b7a026b4d48c0a9d870ce0a34338f577
John Smith <themixmeister@yahoo.com>
2016-10-26 22:08:24 +0100

    Make bin and obj dirs

--- drone/src/BebopFifoStream.c
+++ drone/src/BebopFifoStream.c
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
     else
     {
         begin_streaming();
-        while (alive)
+        while (alive && !failed)
         {
             usleep(50);
         }
